65d8e491701e94b4caa57d98834e655ff4a33c7f
Matt Blackwell <matt.blackwell@engineering.digital.dwp.gov.uk>
2019-05-22 17:49:19 +0100

    Update config.js
    Changed service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Share your information for a Blue Badge application',
+  serviceName: 'Share your PIP or DLA information',
 
   // Default port that prototype runs on
   port: '3000',
