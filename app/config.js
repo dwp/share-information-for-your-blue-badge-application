b35c3283bc229bc5b201340e6f1f3e4b7f5882dc
Stu Bamforth <stuart.bamforth@engineering.digital.dwp.gov.uk>
2019-07-11 10:30:45 +0100

    Amends to Service Name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Share your benefit information with your council',
+  serviceName: 'Share your DLA information',
 
   // Default port that prototype runs on
   port: '3000',
